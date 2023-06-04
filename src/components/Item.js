25a1ba3ad0749992454fdc4c1cc449a65254fce2
Sharon Awuor <sawuor395@gmail.com>
2023-06-04 13:08:53 +0300

    completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,58 @@
 import React from "react";
 
-function Item({ item }) {
-  return (
-    <li className={item.isInCart ? "in-cart" : ""}>
-      <span>{item.name}</span>
-      <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
-        {item.isInCart ? "Remove From" : "Add to"} Cart
+
+  function Item({ item, onUpdateItem, onDeleteItem  }) {
+    // Add function to handle button click
+    function handleAddToCartClick() {
+      // function handleDeleteClick() {
+      //   console.log(item);
+      // }
+
+      // function handleDeleteItem(deletedItem) {
+      //   const updatedItems = item.filter((item) => item.id !== deletedItem.id);
+      //   setItems(updatedItems);
+      // }
+      // add fetch request
+      fetch(`http://localhost:4000/items/${item.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          isInCart: !item.isInCart,
+        }),
+      })
+        .then((r) => r.json())
+        .then((updatedItem) => onUpdateItem(updatedItem));
+    }
+  
+    // Deconstruct the onDeleteItem prop
+
+  function handleDeleteClick() {
+    // Call onDeleteItem, passing the deleted item
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => onDeleteItem(item));
+  }
+
+    return (
+      <li className={item.isInCart ? "in-cart" : ""}>
+        <span>{item.name}</span>
+        <span className="category">{item.category}</span>
+        {/* add the onClick listener */}
+        <button
+          className={item.isInCart ? "remove" : "add"}
+          onClick={handleAddToCartClick}
+        >
+          {item.isInCart ? "Remove From" : "Add to"} Cart
+        </button>
+        <button className="remove" onClick={handleDeleteClick}>
+        Delete
       </button>
-      <button className="remove">Delete</button>
-    </li>
-  );
-}
+      </li>
+    );
+  }
 
 export default Item;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,35 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+   // Update state by passing the array of items to setItems  
+   useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+
+  // add this function!
+  function handleAddItem(newItem) {
+    console.log("In ShoppingList:", newItem);
+  }
+
+  // add this callback function
+  function handleDeleteItem(deletedItem) {
+    console.log("In ShoppingCart:", deletedItem);
+  }
+
+
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -19,14 +48,20 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
+        {/* pass it as a prop to Item */}
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item
+            key={item.id}
+            item={item}
+            onUpdateItem={handleUpdateItem}
+            onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
